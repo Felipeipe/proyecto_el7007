fee9ef5d587195e593f4b1492b14531b0610b987
Felipe González <gonzalezpobletefelipe@gmail.com>
2026-05-26 16:42:25 -0400

    configs

--- config/env_vars.sh
+++ config/env_vars.sh
@@ -17,7 +17,27 @@ mkdir -p ${nnUNet_results}
 
 cp ${CURRENT_PATH}/config/dataset.json ${nnUNet_raw}/Dataset001_oaizib
 
-
+# dependencies
 pip install torch torchvision
 pip install nnunetv2
 pip install --upgrade git+https://github.com/FabianIsensee/hiddenlayer.git
+
+# dataset configuration
+cd ${nnUNet_raw}/Dataset001_oaizib
+gdown --folder "https://drive.google.com/drive/folders/1hrTVO5CikLl2s5zLRynJJUmXIzzkdbc-"
+rm -rf OAIZIB-CM/info.zip OAIZIB-CM/template_atlas.zip
+mv -r OAIZIB-CM/* .
+unzip imagesTr.zip
+rm -rf imagesTr.zip
+unzip imagesTs.zip
+rm -rf imagesTs.zip
+unzip labelsTr.zip
+rm -rf labelsTr.zip
+unzip labelsTs.zip
+rm -rf labelsTs.zip
+
+# preprocessing
+
+nnUNetv2_plan_and_preprocess -d 1
+
+
